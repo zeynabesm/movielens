08a686d845762a4e68795241089ee454060971fc
Zeynab(nooshin) <esmailpoor1991@gmail.com>
2026-07-03 04:40:59 -0700

    Update er_builder.py

--- analysis/er_builder.py
+++ analysis/er_builder.py
@@ -1,1 +1,8 @@
-
+import pandas as pd
+import networkx as nx
+from pathlib import Path
+DATA_PATH = Path("../Data")
+movies = pd.read_csv(DATA_PATH / "movies.csv")
+ratings = pd.read_csv(DATA_PATH / "ratings.csv")
+tags = pd.read_csv(DATA_PATH / "tags.csv")
+links = pd.read_csv(DATA_PATH / "links.csv")
